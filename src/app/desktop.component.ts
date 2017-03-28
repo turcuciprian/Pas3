de8c22a8a77ee3aa3a3d9382be5ed7aed4f204d7
Ciprian Turcu <turcuciprian1@gmail.com>
2017-03-28 19:06:22 +0300

    added 2 way databinding to  desktop page, added new property for that reorganized the structure of the content

--- src/app/desktop.component.ts
+++ src/app/desktop.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./desktop.component.css']
 })
 export class DesktopComponent implements OnInit {
+  dCode: String = '';
 
   constructor() { }
 
