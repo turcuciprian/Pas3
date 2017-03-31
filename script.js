a305bc94a5a1d53b84c8bf3c967d2825b1dddef8
Ciprian Turcu <turcuciprian1@gmail.com>
2017-03-31 14:55:10 +0300

    added get and set functionality set is not working yet get is

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 jQuery(document).ready(function() {
     var sendCode = $("#sendCode");
     var getCode = $("#getCode");
+    var codeCode = $('#codecode');
     var stringData = $("#stringData");
     var stringDataStr = '';
+    var serverurl = "http://localhost:8000/slr";
     if (sendCode[0]) {
         sendCode.click(function() {
             if (stringData[0]) {
@@ -12,14 +14,34 @@ jQuery(document).ready(function() {
 
             $.ajax({
                     method: "POST",
-                    url: "http://localhost:8000/slr",
-                    data: { sc: stringDataStr }
+                    url: serverurl,
+                    // data: { "sc": stringDataStr }
                 })
                 .done(function(msg) {
 
                     console.log("Data Saved: " + msg);
                 });
         });
+    }
+    //get the text
+
+    if (getCode[0]) {
+        getCode.click(function() {
+            $.ajax({
+                    method: "GET",
+                    url: serverurl,
+                    // data: { "sc": stringDataStr }
+                })
+                .done(function(msg) {
+                    var jsonobj = $.parseJSON(msg);
+                    if (codeCode[0]) {
+                        codeCode.html(jsonobj[0].sc);
+                    }
+
+                    console.log("Data recieved: " + msg);
+                });
+        });
+
     }
 
 });
